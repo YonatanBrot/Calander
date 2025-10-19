e56958229eec93ac2280aeaaf6065187b43649cb
YonatanBrot <yonatanbrot@gmail.com>
2025-10-19 10:13:54 +0300

    test for add

--- events.py
+++ events.py
@@ -5,21 +5,20 @@ from arrow import Arrow
 app = typer.Typer()
 
 @app.command()
-def add(name: str, start: str, end:str):
-    with open('events.py', 'r', encoding='utf-8', newline='') as file:
-        pass
-    
-@app.command
-def test(num: int):
-    print(num+1)
+def add(items: list[str] = typer.Argument(..., nargs=-1)):
+    pass
 
 @app.command()
+def test(*args):
+    print(type(args))
+
+'''@app.command()
 def init(name: str, date: str):
     name = name.replace('_', ' ')
     date = [int(n) for n in date.strip().split('/')]
     c = Calendar(events = [Event(name = name, begin = Arrow(date[2], date[1], date[0]))], creator='-//YB/classes//EN')
     with open('events.ics', 'w', encoding='utf-8', newline='') as file:
-        file.writelines(c.serialize_iter())
+        file.writelines(c.serialize_iter())'''
 
 if __name__ == "__main__":
     app()
