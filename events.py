61db782fbe504be384bfedc8e04ad272bdbd95fb
YonatanBrot <yonatanbrot@gmail.com>
2025-10-21 12:09:07 +0300

    added the base, unfixed, add function for full day events

--- events.py
+++ events.py
@@ -5,20 +5,28 @@ from arrow import Arrow
 app = typer.Typer()
 
 @app.command()
-def add(items: list[str] = typer.Argument(..., nargs=-1)):
-    pass
+def add(items: list[str]):
+    try:
+        with open('events.ics', 'r') as file:
+            c = Calendar(file)
+            for item in items:
+                date,name = item.split(';')
+                date = [int(n) for n in date.split('/')]
+                c.events.add(Event(name, Arrow(date[0], date[1], date[2])))
+        with open('events.ics', 'w', encoding='utf-8', newline='') as file:
+            file.writelines(c.serialize_iter())
 
-@app.command()
-def test(*args):
-    print(type(args))
-
-'''@app.command()
-def init(name: str, date: str):
-    name = name.replace('_', ' ')
-    date = [int(n) for n in date.strip().split('/')]
-    c = Calendar(events = [Event(name = name, begin = Arrow(date[2], date[1], date[0]))], creator='-//YB/classes//EN')
-    with open('events.ics', 'w', encoding='utf-8', newline='') as file:
-        file.writelines(c.serialize_iter())'''
+    except FileNotFoundError:
+        c = Calendar()
+        date, name = items[0].split(';')
+        date = [int(n) for n in date.split('/')]
+        c.events.add(Event(name, Arrow(date[0], date[1], date[2])))
+        with open('events.ics', 'w', newline='', encoding='utf-8') as file:
+            file.writelines(c.serialize_iter())
+        try:
+            add(items[1:])
+        except IndexError:
+            pass
 
 if __name__ == "__main__":
     app()
