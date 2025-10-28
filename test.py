87d4547145ff13399ff205017de6a250252e6dbf
YonatanBrot <yonatanbrot@gmail.com>
2025-10-28 18:56:50 +0200

    renamed some files

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-class Parent:
-    def __init__(self, x):
-        self.x = x
-    def func():
-        pass
-
-class Child(Parent):
-    def __init__(self, x):
-        # super().__init__(x)
-        pass
-
-b = Child("a")
-b.func()
+pass
