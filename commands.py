ad0011935896abd2d344dbe0fc5136fd52fb6149
YonatanBrot <yonatanbrot@gmail.com>
2025-11-04 08:21:46 +0200

    functional move command

--- commands.py
+++ commands.py
@@ -1,20 +1,20 @@
 import typer
 from ics import Event, Calendar
 from arrow import Arrow
-from datetime import date, timedelta
+import datetime as dt
 
 app = typer.Typer()
 
 @app.command()
-def add(args: list[str]):
+def add(path: str, args: list[str]):
     try:
-        with open('events.ics', 'r') as file:
+        with open(path, 'r') as file:
             c = Calendar(file)
             for item in args:
                 date,name = item.split(';')
                 date = [int(n) for n in date.split('/')]
                 c.events.add(Event(name, Arrow(date[0], date[1], date[2])))
-        with open('events.ics', 'w', encoding='utf-8', newline='') as file:
+        with open(path, 'w', encoding='utf-8', newline='') as file:
             file.writelines(c.serialize_iter())
 
     except FileNotFoundError:
@@ -33,29 +33,29 @@ def str_to_date(string):
     if '-' in string:
         begin, end = string.split('-')
         begin = [int(n) for n in begin.split('/')]
-        begin = date(begin[2], begin[1], begin[0])
+        begin = dt.date(begin[2], begin[1], begin[0])
         end = [int(n) for n in end.split('/')]
-        end = date(end[2], end[1], end[0])
+        end = dt.date(end[2], end[1], end[0])
         current = begin
         toReturn = [begin]
         while(current!=end):
-            current+=timedelta(1)
+            current+=dt.timedelta(1)
             toReturn.append(current)
         return toReturn
     else:
         time = [int(n) for n in string.split('/')]
-        return [date(time[2], time[1], time[0])]
+        return [dt.date(time[2], time[1], time[0])]
             
 @app.command()
-def remove(args: list[str]):
+def remove(path: str, args: list[str]):
     for item in args:
-        with open('events.ics', 'r') as file:
+        with open(path, 'r') as file:
             c = Calendar(file)
         try:
             date,name = item.split(';')
             date = [int(n) for n in date.split('/')]
             c.events.remove(Event(name, Arrow(date[0], date[1], date[2])))
-            with open('events.py', 'w', newline='', encoding='utf-8') as file:
+            with open(path, 'w', newline='', encoding='utf-8') as file:
                 file.writelines(c.serialize_iter)
         except KeyError:
             print('Event not in calendar')
@@ -67,22 +67,21 @@ def blackout(args: list[str]):
     dates = [str_to_date(i) for i in args]
     
 @app.command()
-def timezone(path: str):
+def move(path: str, offset: int):
+    print(path)
     with open(path, 'r', newline='', encoding='utf-8') as file:
-        file = file.read()
-        c = Calendar(file)
-    dst_start = date(2025, 10, 25)
-    dst_end = date(2026, 3, 28)
-    for event in c.events:
-        if dst_start < event.begin.date() < dst_end:
-            event.begin.time += timedelta(hours=1)
-            event.end.time += timedelta(hours=1)
-    with open(path, 'w', encoding='utf-8', newline='') as file:
+        c = Calendar(file.read())
+    for e in c.events:
+        e.end = e.end.shift(hours=offset)
+        e.begin = e.begin.shift(hours=offset)
+    with open(path, 'w', newline='', encoding='utf-8') as file:
         file.writelines(c.serialize_iter())
+
+
 def testing(path: str):
     with open(path, 'r', newline='', encoding='utf-8') as file:
         c = Calendar(file.read())
     pass
 
 if __name__ == "__main__":
-    testing(r'C:\Users\yonat\Desktop\Code\Python\Personal_projects\Calander_gen\repo\Calander\classes.ics')
+    app()
